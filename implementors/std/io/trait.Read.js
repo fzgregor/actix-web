d2f438d209af2d6518e98fdcb79222f3064e4898
Travis CI User <travis@example.org>
2018-08-17 17:13:16 +0000

    Documentation upload

---
+++ implementors/std/io/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"actix_web/client/struct.Connection.html\" title=\"struct actix_web::client::Connection\">Connection</a>",synthetic:false,types:["actix_web::client::connector::Connection"]},{text:"impl&lt;S:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"actix_web/server/struct.TlsStream.html\" title=\"struct actix_web::server::TlsStream\">TlsStream</a>&lt;S&gt;",synthetic:false,types:["actix_web::server::ssl::nativetls::TlsStream"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/tokio_io/async_write/trait.AsyncWrite.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.7/tokio_io/async_write/trait.AsyncWrite.html\" title=\"trait tokio_io::async_write::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"actix_web/client/struct.Connection.html\" title=\"struct actix_web::client::Connection\">Connection</a>",synthetic:false,types:["actix_web::client::connector::Connection"]},{text:"impl&lt;S:&nbsp;<a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.7/tokio_io/async_read/trait.AsyncRead.html\" title=\"trait tokio_io::async_read::AsyncRead\">AsyncRead</a> + <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.7/tokio_io/async_write/trait.AsyncWrite.html\" title=\"trait tokio_io::async_write::AsyncWrite\">AsyncWrite</a>&gt; <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1.7/tokio_io/async_write/trait.AsyncWrite.html\" title=\"trait tokio_io::async_write::AsyncWrite\">AsyncWrite</a> for <a class=\"struct\" href=\"actix_web/server/struct.TlsStream.html\" title=\"struct actix_web::server::TlsStream\">TlsStream</a>&lt;S&gt;",synthetic:false,types:["actix_web::server::ssl::nativetls::TlsStream"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
